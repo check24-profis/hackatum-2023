7ac986966e06838298eb7d9c9e2fb0c209b56186
Yifei Zhan <yifei.zhan@check24.de>
2023-11-09 15:51:24 +0100

    set up simple node project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#### How to run
-1. git clone
-2. docker-compose
+#### A simple app in NodeJS
+1. for development: use `npm start`
+2. for other purpose: use `docker-compose -d up` to start and `docker-compose stop` to stop
--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const app = express();
-const { getAllTimestamps, addCurrentTimestamp } = require('./src/database-timestamp-table');
 
-// Get all timestamps added
-app.get('/timestamps', async (_, res) => {
-  const result = await getAllTimestamps();
-  res.send(result);
-});
-
-// Add current timestamp to database
-app.post('/current-timestamp', async (_, res) => {
-  const result = await addCurrentTimestamp();
-  res.send(result);
+app.get('/greetings', (_, res) => {
+  res.send("Hello World!");
 });
 
 const port = process.env['SERVER_PORT'] || 3000;
