bf5e9bae923a36384a9db6153edac75a55614676
fjodorgit <f.kholodkov@gmail.com>
2023-11-18 22:00:26 +0100

    fixed min max calculation

--- api/app/migrations/2023-11-18-132021_calculate_min_max_lat/up.sql
+++ api/app/migrations/2023-11-18-132021_calculate_min_max_lat/up.sql
@@ -3,31 +3,31 @@ Update service_provider_profile
 Set
   max_lat_a = degrees(radians(lat) + (max_driving_distance::double precision)/6371000),
   min_lat_a = degrees(radians(lat) - (max_driving_distance::double precision)/6371000),
-  max_lat_b = degrees(radians(lat) + (max_driving_distance::double precision + 2000)/6371000),
-  min_lat_b = radians(lat) - degrees((max_driving_distance + 2000)/6371000),
-  max_lat_c = radians(lat) + degrees((max_driving_distance + 5000)/6371000),
-  min_lat_c = radians(lat) - degrees((max_driving_distance + 5000)/6371000),
+  max_lat_b = degrees(radians(lat) + ((max_driving_distance::double precision + 2000)/6371000)),
+  min_lat_b = degrees(radians(lat) - ((max_driving_distance::double precision + 2000)/6371000)),
+  max_lat_c = degrees(radians(lat) + ((max_driving_distance::double precision + 5000)/6371000)),
+  min_lat_c = degrees(radians(lat) - ((max_driving_distance::double precision + 5000)/6371000)),
 
   max_lon_a = lon + degrees(asin( 
-    ((sin(max_driving_distance/6371000) / cos(radians(lat)))
-  ))),
-  
+    sin((max_driving_distance::double precision)/6371000) / cos(radians(lat))
+  )),
+
   min_lon_a = lon - degrees(asin( 
-    ((sin(max_driving_distance/6371000) / cos(radians(lat)))
-  ))),
-  
+    sin((max_driving_distance::double precision)/6371000) / cos(radians(lat))
+  )),
+
   max_lon_b = lon + degrees(asin( 
-    ((sin((max_driving_distance + 2000)/6371000) / cos(radians(lat)))
-  ))),
-  
+    sin((max_driving_distance::double precision + 2000)/6371000) / cos(radians(lat))
+  )),
+
   min_lon_b = lon - degrees(asin( 
-    ((sin((max_driving_distance + 2000)/6371000) / cos(radians(lat)))
-  ))),
-  
+    sin((max_driving_distance::double precision + 2000)/6371000) / cos(radians(lat))
+  )),
+
   max_lon_c = lon + degrees(asin( 
-    ((sin((max_driving_distance + 5000)/6371000) / cos(radians(lat)))
-  ))),
-  
-  min_lon_c = lon - degrees(asin( 
-    ((sin((max_driving_distance + 5000)/6371000) / cos(radians(lat)))
-  )));
+    sin((max_driving_distance::double precision + 5000)/6371000) / cos(radians(lat))
+  )),
+
+  min_lon_c = lon - degrees(asin(
+    sin((max_driving_distance::double precision + 5000)/6371000) / cos(radians(lat))
+  ));
