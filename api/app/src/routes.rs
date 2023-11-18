90eb9e20e3328a6fb326878939c291d845311e11
fjodorgit <f.kholodkov@gmail.com>
2023-11-18 22:28:15 +0100

    fixed routes again

--- api/app/src/routes.rs
+++ api/app/src/routes.rs
@@ -15,67 +15,58 @@ use diesel::r2d2::Pool;
 
 type DbPool = Pool<ConnectionManager<PgConnection>>;
 
-
-
-
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-    cfg
-    .service(
-        web::resource("/craftsmen")
-        .route(web::get().to(getCraftsmen))
-    )
-    .service(
-        web::resource("/craftman/{craftman_id}")
-        .route(web::patch().to(index))
-    )
-    ;
+    cfg.service(web::resource("/").route(web::get().to(hello)))
+        .service(web::resource("/echo").route(web::post().to(echo)))
+        .service(web::resource("/craftsmen").route(web::get().to(getCraftsmen)))
+        .service(web::resource("/craftman/{craftman_id}").route(web::patch().to(index)))
+        .service(web::resource("/craftsman").route(web::get().to(get_top20_craftmen)));
 }
-
-
 
 #[derive(Debug, Deserialize)]
 pub struct PatchRequest {
-  // At least one of the attributes should be defined
-  maxDrivingDistance: Option<i32>,
-  profilePictureScore: Option<f64>,
-  profileDescriptionScore: Option<f64>
+    // At least one of the attributes should be defined
+    maxDrivingDistance: Option<i32>,
+    profilePictureScore: Option<f64>,
+    profileDescriptionScore: Option<f64>,
 }
 
 async fn index(
-    pool: web:: Data<DbPool>,
+    pool: web::Data<DbPool>,
     craftman_id: web::Path<i32>,
-    req_body: web::Json<PatchRequest>
+    req_body: web::Json<PatchRequest>,
 ) -> actix_web::Result<impl Responder> {
-
     let updatedScore = web::block(move || {
         // So, it should be called within the `web::block` closure, as well.
         let mut conn = pool.get().expect("couldn't get db connection from pool");
-        
+
         updateCraftman(&mut conn, craftman_id, req_body)
-        
-    }).await?;
+    })
+    .await?;
     //.map_err(error::ErrorInternalServerError)?;
 
-        
     Ok(HttpResponse::Ok().json(updatedScore))
-
 }
 
 #[derive(Debug, Serialize)]
 pub struct Updated {
-  // At least one of the attributes should be defined
-  maxDrivingDistance: i32,
-  profilePictureScore: f64,
-  profileDescriptionScore: f64
+    // At least one of the attributes should be defined
+    maxDrivingDistance: i32,
+    profilePictureScore: f64,
+    profileDescriptionScore: f64,
 }
 
 #[derive(Debug, Serialize)]
 struct PatchResponse {
     id: i32,
-    updated: Updated
+    updated: Updated,
 }
 
-pub fn updateCraftman(conn: &mut PgConnection, craftman_id: web::Path<i32>, req_body: web::Json<PatchRequest>) -> PatchResponse {
+pub fn updateCraftman(
+    conn: &mut PgConnection,
+    craftman_id: web::Path<i32>,
+    req_body: web::Json<PatchRequest>,
+) -> PatchResponse {
     let PatchRequest {
         maxDrivingDistance,
         profilePictureScore,
@@ -85,37 +76,41 @@ pub fn updateCraftman(conn: &mut PgConnection, craftman_id: web::Path<i32>, req_
     let craftman_id = craftman_id.into_inner();
 
     let mut score: NewQualityFactorScore;
-    let craftsman: ServiceProviderProfile; 
+    let craftsman: ServiceProviderProfile;
 
     if maxDrivingDistance.is_some() {
         craftsman = diesel::update(service_provider_profile.filter(id.eq(&craftman_id)))
-        .set(max_driving_distance.eq(maxDrivingDistance.unwrap()))   
-        .get_result(conn)
-        .expect("Error updating craftsman");
+            .set(max_driving_distance.eq(maxDrivingDistance.unwrap()))
+            .get_result(conn)
+            .expect("Error updating craftsman");
     } else {
-        craftsman = service_provider_profile.filter(id.eq(&craftman_id)).first(conn).expect("Error loading craftsman");
+        craftsman = service_provider_profile
+            .filter(id.eq(&craftman_id))
+            .first(conn)
+            .expect("Error loading craftsman");
     }
     if profileDescriptionScore.is_none() && profilePictureScore.is_none() {
-        score = quality_factor_score.filter(profile_id.eq(&craftman_id)).first(conn).expect("Error loading score")
-    }
-    else if profilePictureScore.is_some() {
+        score = quality_factor_score
+            .filter(profile_id.eq(&craftman_id))
+            .first(conn)
+            .expect("Error loading score")
+    } else if profilePictureScore.is_some() {
         score = diesel::update(quality_factor_score.filter(profile_id.eq(&craftman_id)))
-        .set(profile_picture_score.eq(profilePictureScore.unwrap()))
-        .get_result(conn)
-        .expect("Error updating score");
-    }
-    else { // profileDescriptionScore.is_some()
+            .set(profile_picture_score.eq(profilePictureScore.unwrap()))
+            .get_result(conn)
+            .expect("Error updating score");
+    } else {
+        // profileDescriptionScore.is_some()
         score = diesel::update(quality_factor_score.filter(profile_id.eq(&craftman_id)))
-        .set(profile_description_score.eq(profileDescriptionScore.unwrap()))   
-        .get_result(conn)
-        .expect("Error updating score");
+            .set(profile_description_score.eq(profileDescriptionScore.unwrap()))
+            .get_result(conn)
+            .expect("Error updating score");
     }
-    
-   
+
     let updated = Updated {
         maxDrivingDistance: craftsman.max_driving_distance.unwrap(),
         profilePictureScore: score.profile_picture_score,
-        profileDescriptionScore: score.profile_description_score
+        profileDescriptionScore: score.profile_description_score,
     };
     let patchResponse = PatchResponse {
         id: craftman_id,
@@ -141,3 +136,4 @@ pub async fn get_top20_craftmen(
     let toptwenty = get_top_20_craftsmen(postal_code, &mut conn);
     Ok(web::Json(toptwenty))
 }
+
