76c368d53bccd49f943554c28ef365d72e0ed7d4
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2023-11-18 12:09:00 +0100

    Progress

--- check24-hackatum-client/src/routes/+layout.svelte
+++ check24-hackatum-client/src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
     import "../app.css";
-    import 'material-icons/iconfont/material-icons.css';
 </script>
 
 <!-- Navbar -->
-<div class="bg-[#063773] text-white p-4 flex justify-center">
+<div class="bg-24-blue text-white p-4 flex justify-center">
     <img src="/img/craft24_logo.svg"
         alt="Craft24 Logo"
         class="h-6">
--- check24-hackatum-client/src/routes/+page.svelte
+++ check24-hackatum-client/src/routes/+page.svelte
@@ -1,10 +1,29 @@
+<script>
+    import Textfield from '@smui/textfield';
+
+    let query = "";
+</script>
+
 <h1 class="text-2xl"><b>Handwerker</b> - Profis in Ihrer Nähe</h1>
-<p class="">
-    Kostenlos Angebote innerhalb weniger Stunden erhalten.
-    
-    Professionelle Handwerkerleistungen für Ihr Projekt
-    
-    Maler, Elektriker, Fliesenleger & andere Handwerker
-    
-    Bequem und sicher online zahlen
-</p>
+<span class="text-[#008300]">Kostenlos Angebote innerhalb weniger Stunden erhalten.</span>
+
+<div class="flex flex-row items-center gap-2">
+    <span class="material-symbols-rounded text-24-blue">done</span> Professionelle Handwerkerleistungen für Ihr Projekt
+</div>
+<div class="flex flex-row items-center gap-2">
+    <span class="material-symbols-rounded text-24-blue">done</span> Maler, Elektriker, Fliesenleger & andere Handwerker
+</div>
+<div class="flex flex-row items-center gap-2">
+    <span class="material-symbols-rounded text-24-blue">done</span> Bequem und sicher online zahlen
+</div>
+
+<span class="block mt-3 mb-2">In welcher PLZ suchen Sie einen Handwerker?</span>
+<Textfield
+    variant="filled"
+    label="Ihre PLZ"
+    class="w-full"
+    bind:value={query} 
+>
+    <span class="material-symbols-rounded ml-[16px] mr-[8px] text-xl" slot="leadingIcon">home_pin</span>
+</Textfield>
+<!-- Button to redirect to new page -->
