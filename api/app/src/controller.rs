611d807f2d698b2e8e6ba3526e37b33e2b8a124f
fjodorgit <f.kholodkov@gmail.com>
2023-11-18 11:58:20 +0100

    fixed merge conflict

--- api/app/src/controller.rs
+++ api/app/src/controller.rs
@@ -1,5 +1,5 @@
 use actix_web::{web, HttpResponse, Responder};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 pub async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -9,6 +9,8 @@ pub async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+// TODO
+//change json req structure
 pub async fn getCraftsmen(req_body: String) -> impl Responder {
     // TODO
     // fetch from database
@@ -27,3 +29,25 @@ pub async fn getCraftsmen(req_body: String) -> impl Responder {
     }
 }
 
+#[derive(Debug, Deserialize)]
+pub struct PatchRequest {
+    // At least one of the attributes should be defined
+    maxDrivingDistance: i32,
+    profilePictureScore: f64,
+    profileDescriptionScore: f64,
+}
+
+pub async fn updateCraftman(
+    craftman_id: web::Path<String>,
+    req_body: web::Json<PatchRequest>,
+) -> impl Responder {
+    // TODO
+    // implement
+
+    // For testing only
+    // TODO: delete
+    println!("called");
+    let response = format!("You passed the id: {}", craftman_id);
+
+    HttpResponse::Ok().body(response)
+}
