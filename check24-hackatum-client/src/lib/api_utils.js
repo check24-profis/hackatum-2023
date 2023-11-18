e8d8dc0556e4cc196ba4ac8107a5898f6d5e1a4d
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2023-11-18 22:00:45 +0100

    Frontend Progress

---
+++ check24-hackatum-client/src/lib/api_utils.js
@@ -0,0 +1,18 @@
+async function api_fetch(domain, url_appendix) {
+    let port = 8080;
+    let url = `${domain}:${port}/${url_appendix}`;
+    let response = await fetch(url);
+    if(response.ok) {
+        return response.json();
+    } else {
+        console.error(`Error while fetching ${url_appendix} ->`, response);
+        return {};
+    }
+}
+
+export async function fetch_craftsmen(domain, plz, page, load_state_update) {
+    load_state_update(true);
+    let response_json = await api_fetch(domain, `craftsmen?postalcode=${plz}&page=${page}`, load_state_update);
+    load_state_update(false);
+    return response_json['data'] ?? [];
+}
